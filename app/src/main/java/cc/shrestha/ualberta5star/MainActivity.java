a503caa4eb661912961c9535bee041dfaa30705e
anjesh12 <anjeshshrestha@gmail.com>
2019-01-19 21:04:40 -0700

    Fixed Clear text HTTP

--- app/src/main/java/cc/shrestha/ualberta5star/MainActivity.java
+++ app/src/main/java/cc/shrestha/ualberta5star/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //initializing the productlist
+        //initializing the buildingList
         buildingList = new ArrayList<>();
 
 
@@ -43,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadBuildings() {
 
-
         /*
          * Creating a String Request
          * The request type is GET defined by first parameter
@@ -51,49 +51,43 @@ public class MainActivity extends AppCompatActivity {
          * Then we have a Response Listener and a Error Listener
          * In response listener we will get the JSON response as a String
          * */
-        Log.d("myTag","this should work very much");
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_PRODUCTS,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
 
-                            //converting the string to json array object
-                            JSONArray array = new JSONArray(response);
-                            //traversing through all the object
-                            for (int i = 0; i < array.length(); i++) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL_PRODUCTS, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("Stuff","working");
+                    JSONArray jsonArray = response.getJSONArray("");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        Log.d("Stuff","working");
+                        //getting product object from json array
+                        JSONObject building = jsonArray.getJSONObject(i);
 
-                                //getting product object from json array
-                                JSONObject building = array.getJSONObject(i);
-
-                                //Log.d("Stuff",building.get("shortdesc").toString());
-
-                                //adding the product to product list
-                                buildingList.add(new Building(
-                                        building.getInt("id"),
-                                        building.getString("name"),
-                                        building.getString("shortdesc"),
-                                        building.getDouble("rating"),
-                                        building.getString("image")
-                                ));
-                            }
-
-                            //creating adapter object and setting it to recyclerview
-                            BuildingAdapter adapter = new BuildingAdapter(MainActivity.this, buildingList);
-                            recyclerView.setAdapter(adapter);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        int id = building.getInt("id");
+                        String name = building.getString("name");
+                        String shortdesc = building.getString("shortdesc");
+                        double rating = building.getDouble("rating");
+                        String image = building.getString("image");
+                        //adding the product to product list
+                        buildingList.add(new Building(id, name, shortdesc,rating,image));
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
 
-                    }
-                });
-
+                    //creating adapter object and setting it to recyclerview
+                    BuildingAdapter adapter = new BuildingAdapter(MainActivity.this, buildingList);
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Stuff","not working");
+                error.printStackTrace();
+            }
+        });
         //adding our stringrequest to queue
-        Volley.newRequestQueue(this).add(stringRequest);
+        Volley.newRequestQueue(this).add(request);
+
     }
 }
